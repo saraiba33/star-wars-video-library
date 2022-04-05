db0535a10c809ea4b6099211f1796af5dc648104
aconkli9 <aconkli9@ford.com>
2022-04-05 11:52:38 -0400

    Add movie listings

--- site/index.js
+++ site/index.js
@@ -1,10 +1,28 @@
 const url = "https://swapi.dev/api/films";
 
+function createDiv(parsedObject) {
+    const div = document.createElement("div")
+    div.classList.add("movie-listing")
+    div.innerHTML = `<a href=> ${parsedObject.title}</a>
+    <time>${parsedObject.release_date}</time>
+    `
+    const ul = document.querySelector("ul")
+    ul.append(div)
+}
 
 fetch(url)
     .then(response => {
         return response.json()
     }).then(parsedResponse => {
         const movies = parsedResponse.results
-        console.log(movies)
+        const movieUrls = movies.map(movie => movie.url)
+        const movieFetches = movieUrls.map(url => fetch(url)
+            .then(response => response.json()))
+        return Promise.all(movieFetches)
+            .then(parsedResponse => {
+                console.log(parsedResponse)
+                parsedResponse.forEach(parsedResponse => {
+                    createDiv(parsedResponse)
+                })
+            })
     })
