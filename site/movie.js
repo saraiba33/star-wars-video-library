f1f7dce2e7e6dc5ea2efb6abaa124b4813ab4c07
Sara Ibarra <saraiba33@gmail.com>
2022-04-05 12:58:16 -0600

    add characters

--- site/movie.js
+++ site/movie.js
@@ -9,12 +9,45 @@ const starWarsIdMap = {
     6: 3
 }
 
+
 const newId = starWarsIdMap[queryString.get("films")]
-console.log(newId)
 
 fetch(`https://swapi.dev/api/films/${newId}`)
     .then(response => {
         return response.json()
     }).then(parsedResponse => {
         console.log(parsedResponse)
+        createDiv(parsedResponse)
+        createCharacterList(parsedResponse)
     })
+
+function createCharacterList(parsedObject) {
+    const characterUrls = parsedObject.characters
+    characterUrls.forEach(characterUrl => {
+        fetch(characterUrl).then(response => {
+            return response.json()
+        }).then(parsedResponse => {
+            const li = document.createElement("li")
+            li.textContent = parsedResponse.name
+            const ul = document.querySelector(".characters")
+            ul.append(li)
+        })
+
+    });
+    console.log(characterUrls)
+}
+
+
+
+function createDiv(parsedObject) {
+    const div = document.createElement("div")
+    div.classList.add("movie")
+    div.innerHTML = `<a href=> ${parsedObject.title}</a>
+    <time>${parsedObject.release_date}</time>
+    <h2>Characters</h2>
+    <ul class="characters"></ul>
+    `
+    const main = document.querySelector("main")
+    main.append(div)
+
+}
